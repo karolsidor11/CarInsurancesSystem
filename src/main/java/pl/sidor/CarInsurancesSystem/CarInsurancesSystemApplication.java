165fed0a049647d0c5aaa1d4d8a87bcbd0f23efc
KursJava <kursjava1@wp.pl>
2019-12-30 16:59:48 +0100

    Git init

---
+++ src/main/java/pl/sidor/CarInsurancesSystem/CarInsurancesSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sidor.CarInsurancesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsurancesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInsurancesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sidor/CarInsurancesSystem/CarInsurancesSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sidor.CarInsurancesSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarInsurancesSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
